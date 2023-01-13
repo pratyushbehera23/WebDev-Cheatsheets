fef4f16ce1f65538c6916c350c8addc73b06c931
pratyushbehera23 <pratyushbehera.mail@gmail.com>
2023-01-13 16:12:09 +0530

    added TypeScript notes

---
+++ index.ts
@@ -0,0 +1,48 @@
+// TypeScript notes 
+/*
+    Data Types
+    Unions
+    Interface
+    ...
+*/
+
+// Types 
+const myName:string = 'Pratyush';
+let id:number = 23;
+let isDev:boolean = true;
+
+
+// Unions 
+let gender: 'male'|'female';
+gender = 'male';
+
+
+// Interface 
+interface Person{
+    name:string
+    age:number
+    gender:'male'|'female'
+    isFit?:boolean
+}
+
+let naruto:Person = {
+    name:'Naruto',
+    age:27,
+    gender:'male',
+    // isFit:true
+}
+
+let sasuke:Partial<Person> = {
+    name:'Sasuke',
+    gender:'male'
+}
+
+
+// Functions
+const sum = (x:number, y:number):number => {
+    return x+y;
+}
+sum(2,3);
+
+
+// 
